6720ad07ffb4a3114439d3a08f070d020795f031
Andres David <kyfunky1386@gmail.com>
2024-08-05 22:17:02 -0500

    trabajando en las configuración de las views

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
 const express = require("express");
+const path = require('path');
 const app = express();
+const expressLayaouts = require('express-ejs-layouts');
 
+// Configuración de EJS
+app.set('view engine','ejs');
+app.use(expressLayaouts);
+app.set('views',__dirname+'/views');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended:true}));
+
+// Archivos estáticos
+app.use(express.static(__dirname + "/public"));
+
+
+// Rutas
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Inicio' });
+});
+
+// Iniciar servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
